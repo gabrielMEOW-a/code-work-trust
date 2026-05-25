60a59198df2ae609b7d89af88b3692d1efa3df17
gabrielMEOW-a <gabrielluchen@outlook.com>
2026-05-24 18:37:28 -0600

    gud code

---
+++ include/subsystems.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+inline pros::Motor dr4bl(15);
+inline pros::Motor dr4br(-16);
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
